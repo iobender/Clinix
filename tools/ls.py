b1f0646d5cb0ffba11afe02f438729c35b111fbe
Matthew Bender <me@iobender.com>
2015-06-10 00:16:26 -0400

    updated ls to return only 1 element if only 1 entry was given

--- tools/ls.py
+++ tools/ls.py
@@ -9,10 +9,18 @@ def ls(*entries, **options):
 
     lists directory contents
 
-    args should be a list of files/directories to process
-    if args is empty, a sole argument '.' is assumed
+    Entries should be a list of files/directories to process
+    if entries is empty, a sole argument '.' is assumed
 
-    kwargs is a dict of options to ls
+    A list is returned, with elements as follows:
+    if an entry was a file, just the filename 
+    if an entry is a directory, a tuple of filename and list
+    of files/directories in that directory 
+
+    If only 1 entry is given (or none in which case the one entry),
+    then a list is not returned, and instead just that one element
+
+    options is a dict of options to ls
     Valid options (with defaults):
         
     """
@@ -22,7 +30,11 @@ def ls(*entries, **options):
     if not entries:
         entries = ['.']
 
-    result = [_ls_entry(entry, **options) for entry in entries]
+    if len(entries) == 1:
+        result = _ls_entry(entries[0], **options)
+    else:
+        result = [_ls_entry(entry, **options) for entry in entries]
+
     return result
 
 def _ls_entry(entry, **options):
